f1a5681023aab8907f8145b83a886dd3be1a9c27
Vikash Sharma <vikashsharma8914@gmail.com>
2026-03-04 14:14:45 +0530

    Updated python files

---
+++ Python Test/Condition.py
@@ -0,0 +1,7 @@
+age = int (input ("What is your age? "))
+
+if age >= 18:
+    print ("You are eligible for admission")
+
+else:
+    print ("Better luck next time")
---
+++ Python Test/condition2.py
@@ -0,0 +1,8 @@
+number1 = int (input("Enter a number: "))
+number2 = int (input("Enter another number: "))
+#number3 = int (input("Enter another number: "))
+
+if number1 < number2:
+    print (number1)
+else :
+    print (number2 , "this is the correct answer")
