2d5754506de71b8601a817a9b5dd942c7128777a
Vikash Sharma <vikashsharma8914@gmail.com>
2026-03-07 14:16:31 +0530

    Learn OOPS Concept here

---
+++ Python Test/Classes.py
@@ -0,0 +1,13 @@
+class BankAccount:
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+        def deposit(self, amount):
+            self.balance += amount
+
+
+#creating object
+account1 = BankAccount('', 10000)
+print(account1.name)
+print(account1.balance)
---
+++ Python Test/OPPS/lAB01.py
@@ -0,0 +1,9 @@
+class Car:
+    color = "Black"
+    brand = "Mercedes"
+    location = "Jaipur"
+
+car1 = Car()
+print(car1.color)
+print(car1.brand)
+print(car1.location)
